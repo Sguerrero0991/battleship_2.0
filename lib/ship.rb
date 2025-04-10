219369ef5e30e414e6604dbbdd73fb7215afcf6d
William Ross <bill_87_2@yahoo.com>
2025-04-10 16:14:51 -0500

    Ship initialize

---
+++ lib/ship.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+class Ship
+  attr_reader :name, :length, :health
+
+  def initialize(name, length)
+    @name = name
+    @length = length
+    @health = length
+  end
+
+  def hit
+    
+  end
+
+end
