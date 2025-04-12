18980ac859b9b1ca7eec9a12e861a6be97cef4fa
William Ross <bill_87_2@yahoo.com>
2025-04-12 11:28:33 -0500

    Tests for board_spec

---
+++ spec/board_spec.rb
@@ -0,0 +1,22 @@
+require './lib/board'
+require 'pry'
+require 'rspec'
+
+RSpec.describe Board do
+
+  it 'exists' do
+    board = Board.new
+
+    expect(board).to be_a(Board)
+
+  end
+
+  it ' generates cells' do
+    board = Board.new
+    board.cells
+
+    expect(board.cells).to be_a(Hash)
+    expect(board.cells.size).to eq(16)
+  end
+
+end
